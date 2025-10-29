0ee40b5cb741d549e0b403fe143ff7d0bd492dc1
p-junseo <belsono47@gmail.com>
2025-10-29 20:54:37 +0900

    fix(ejs): 변수 변경

--- routes/pages/yourpage.js
+++ routes/pages/yourpage.js
@@ -5,14 +5,13 @@ const pageData = {
     page: {
         title: "Coming Soon",
         description: "페이지가 준비 중입니다. 이메일을 등록하고 가장 먼저 업데이트를 확인하세요.",
-        domainName: "yourpage",
+        domainName: "yourpage.com",
         logoPathName: "/image.png"
     },
     theme: {
-        bodyClass: "bg-blue-50 text-gray-800",
-        logoSvgPath: "M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z",
-        headingGradient: "from-blue-500 to-teal-400",
-        descriptionClass: "text-gray-600",
+        titleColor: "blue",
+        backgroundColor: "black",
+        descriptionColor: "gray",
         buttonColor: "blue"
     }
 };
